3753b4628faf8f6f5e846451499d845bd9e45429
Patrick <boatengpato.pb@gmail.com>
2022-06-30 11:43:28 -0700

    Betty can worry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,12 @@
 
 
 
-int main() {
+int main(void) 
+{
 
 
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
-	return 0;
+	return (0);
 }
 
