38567160185af93b473611e6bd8e85250c7bb049
Patrick <boatengpato.pb@gmail.com>
2022-07-02 05:31:24 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int first_num;
+
+	for (first_num = '0'; first_num <= '9'; first_num++)
+	{
+		putchar(first_num);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
