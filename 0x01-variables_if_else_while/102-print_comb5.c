46e3a49cfc2c27cee69df14cf13960882bd45147
Patrick <boatengpato.pb@gmail.com>
2022-07-04 04:17:47 -0700

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	int i, j, k, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '8'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (l = '0'; l <= '9'; l++)
+				{
+					if (i == k && j == l)
+						continue;
+					if (k == '0' && l == '0')
+						continue;
+
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+
+					if (i == '9' && j == '8' && k == '9' && l == '9')
+					{
+						continue;
+					}
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
