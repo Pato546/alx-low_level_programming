a6648f1fa8e4d37f7f2340ca9157568216e940ef
Patrick <boatengpato.pb@gmail.com>
2022-07-05 13:51:39 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _abs - returns the abolute of an number
+*
+* @int: number
+*
+* Return: int
+*/
+
+int _abs(int)
+{
+
+	if (int < 0)
+	{
+		return (-1 * int);
+	}
+
+	return (int);
+}
+
