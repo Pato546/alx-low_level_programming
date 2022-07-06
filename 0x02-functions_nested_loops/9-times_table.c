900accd1487ded88d6d729105b260152e83b46d4
Patrick <boatengpato.pb@gmail.com>
2022-07-06 14:19:54 -0700

    Times

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* times_table - times
+*
+* Return: void
+*/
+
+int times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int prod = i * j;
+
+			if (prod > 9)
+			{
+				_putchar(prod / 10 + '0');
+			}
+			_putchar(prod % 10 + '0');
+
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+
+
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int print_last_digit(int c);
 void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
+void times_table(void);
 
