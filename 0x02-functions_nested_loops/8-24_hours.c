a80060142d71a6061fbf46727860bcc6fe3d321a
Patrick <boatengpato.pb@gmail.com>
2022-07-06 14:59:07 -0700

    8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,33 +9,19 @@
 void jack_bauer(void)
 {
 
-	int i, j, k, l;
+	int i, j;
 
-	for (i = '0'; i <= '2'; i++)
+	for (i = 0; i <= 23; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j <= 59; j++)
 		{
-			for (k = '0'; k <= '5'; k++)
-			{
-				for (l = '0'; l <= '9'; l++)
-				{
-					_putchar(i);
-					_putchar(j);
-
-					_putchar(':');
-
-					_putchar(k);
-					_putchar(l);
-
-					_putchar('\n');
-
-					if (i == '2' && j == '3' && k == '5' && l == '9')
-					{
-						break;
-					}
-				}
-			}
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }
 
