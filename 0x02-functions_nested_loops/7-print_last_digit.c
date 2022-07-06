dfd60d9c01d72aaf2ce33005aead885f0bb0e867
Patrick <boatengpato.pb@gmail.com>
2022-07-05 21:13:41 -0700

    Last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,17 @@
 
 int print_last_digit(int c)
 {
+	int last_digit;
+
 	if (c < 0)
 	{
 		c *= -1;
 	}
-	return (c % 10);
+
+	last_digit = c % 10;
+
+	_putchar(last_digit);
+
+	return (last_digit);
 }
 
