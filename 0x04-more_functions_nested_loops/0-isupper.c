370819dbe467dcd33cde2abccd05aa8e96fd77ff
Patrick <boatengpato.pb@gmail.com>
2022-07-07 16:15:40 -0700

    Uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+
+/**
+* _isupper - Checks if a character is uppercase
+* @c: character
+* Return: Always 0 (Success)
+*/
+
+int _isupper(int c)
+{
+	if (c >= '65' && c <= '90')
+		return (1);
+
+	return (0);
+
+	return (0);
+}
+
