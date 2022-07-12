3a52fa86ddf86badfe4a02f69de691f47350ae06
Patrick <boatengpato.pb@gmail.com>
2022-07-12 12:46:48 -0700

    Half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+
+/**
+* puts_half - print half of the string;
+* @s: pointer to string
+*
+* Return: int
+*/
+
+void puts_half(char *s)
+{
+	int count;
+
+	count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	half_str = count % 2 == 0 ? count / 2 : (count - 1) / 2;
+
+	while (s[half_str] != '\0')
+	{
+		_putchar(s[half_str]);
+		half_str++;
+	}
+}
+
