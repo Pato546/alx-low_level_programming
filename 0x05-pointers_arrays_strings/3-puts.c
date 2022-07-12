ce269a23c792d7edff4c3a9b877781c62d43841d
Patrick <boatengpato.pb@gmail.com>
2022-07-12 10:03:47 -0700

    Print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts - prints a string;
+* @s: pointer to string
+*
+* Return: void
+*/
+
+void _puts(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+
+	_putchar('\n');
+}
+
