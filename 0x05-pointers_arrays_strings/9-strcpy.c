eb2597d299fe3f5973cfc1069d94e2b3a1488ba6
Patrick <boatengpato.pb@gmail.com>
2022-07-12 13:54:20 -0700

    PC

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+
+/**
+* _strcpy - copy one pointer to the other
+* @dest: pointer 1;
+* @src: pointer 2;
+* Return: int
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int count, i;
+
+	count = 0;
+
+	while (src[count] != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	for (i = 0; i <= count; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+
