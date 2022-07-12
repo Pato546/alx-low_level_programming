269283aba77ec0a7b0f46ac5fa1f00ad577735f6
Patrick <boatengpato.pb@gmail.com>
2022-07-12 10:16:06 -0700

    Print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_rev - print string in reverse order;
+* @s: pointer to string
+*
+* Return: int
+*/
+
+int print_rev(char *s)
+{
+	int str_len;
+
+	str_len = 0;
+
+	while (*s != '\0')
+	{
+		str_len++;
+		s++;
+	}
+
+	str_len -= 1;
+
+	while (str_len != -1)
+	{
+		_putchar(s[str_len]);
+		str_len -= 1;
+	}
+	_putchar('\n')
+}
+
