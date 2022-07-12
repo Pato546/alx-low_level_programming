297bf96a7148324d554d421ccdceebda97888cb7
Patrick <boatengpato.pb@gmail.com>
2022-07-12 13:34:25 -0700

    Array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,12 +16,14 @@ void print_array(int *a, int n)
 	{
 		if (*(a + i) == *(a + (n - 1)))
 		{
-			printf("%d ", *(a + i));
+			printf("%d", *(a + i));
 		}
 		else
 		{
 			printf("%d, ", *(a + i));
 		}
 	}
+
+	putchar('\n');
 }
 
