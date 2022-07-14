0b992ef0cf7da0fca9effc9ac1d0377864eb9dc3
Patrick <boatengpato.pb@gmail.com>
2022-07-14 12:02:39 -0700

    N copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+
+/**
+* _strncpy - copy one string to the other;
+* @dest: pointer 1;
+* @src: pointer 2;
+* @n: number of bytes to copy
+* Return: pointer
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
