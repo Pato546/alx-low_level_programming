9a47765c24ec3761e33b83e60984bf64b1ad380b
Patrick <boatengpato.pb@gmail.com>
2022-07-14 15:08:11 -0700

    Change case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+
+/**
+* string_toupper - convert lowercase letters to uppercase
+* @s: string
+* Return: pointer
+*/
+
+char *string_toupper(char *s)
+{
+	char *start = s;
+
+	while (*s <= 'a' && *s <= 'z')
+	{
+		*s -= 32;
+		s++
+	}
+
+	return (start);
+}
