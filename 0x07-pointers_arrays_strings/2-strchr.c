a9e5dc5ebad29a8636c56cf6f2824f0221b9fd90
Patrick <boatengpato.pb@gmail.com>
2022-07-19 12:15:27 -0700

    Strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+
+/**
+* _strchr - Returns pointer to the char in pointer
+* @s: pointer
+* @c: character
+* Return: pointer
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (s);
+}
+
