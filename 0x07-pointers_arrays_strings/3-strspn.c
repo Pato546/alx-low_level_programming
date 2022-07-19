3bf5053393dee5cc4955ec666140d62f92ea6481
Patrick <boatengpato.pb@gmail.com>
2022-07-19 16:34:55 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,23 +8,43 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int substr_counter, i;
+	unsigned int substr_counter, i, j;
+	unsigned int s_len, accept_len;
 
 	substr_counter = 0;
 	i = 0;
+	j = 0;
+	s_len = 0;
+	accept_len = 0;
 
-	while (s[i])
+	while (s[s_len])
 	{
-		if (s[i] ==	accept[i])
+		s_len++;
+	}
+
+	while (accept[accept_len])
+	{
+		accept_len++;
+	}
+
+	for (i = 0; i < accept_len; i++)
+	{
+
+		for (j = 0; j < s_len; j++)
 		{
-			substr_counter++;
+			if (accept[i] == s[j])
+			{
+				substr_counter++;
+				break;
+			}
 		}
-		else
+		if (j == s_len)
 		{
 			return (substr_counter);
 		}
-		i++;
 	}
+
 	return (substr_counter);
+
 }
 
