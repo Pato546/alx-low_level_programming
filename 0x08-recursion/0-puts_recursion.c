8358eb6577e4a6ed7541ce671102c68d6634a520
Patrick <boatengpato.pb@gmail.com>
2022-07-20 11:56:54 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _puts_recursion - prints string
+* @s: character string
+* Return: Always 0 (Success)
+*/
+
+void _puts_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	_putchar(*s);
+
+	_puts_recursion(s++);
+}
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+_putchar(char n);
+_puts_recursion(char *s);
+
+#endif
+
