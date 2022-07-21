78e74633f4d305c46d2c7290c5bb85055ca892c2
Patrick <boatengpato.pb@gmail.com>
2022-07-21 04:45:40 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+
+/**
+* _strlen_recursion - returns length of a string
+* @s: string
+* Return: int
+*/
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s)
+	{
+		return (0);
+	}
+
+	s++;
+
+	return (1 + _strlen_recursion(s))
+}
+
